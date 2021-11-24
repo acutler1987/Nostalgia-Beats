678d12cb04a4d28de605d0744152ae90054b8649
Alice Cutler <acutler0451@gmail.com>
2021-11-23 19:23:33 -0500

    starting to implement API calls

--- app.js
+++ app.js
@@ -1,11 +1,105 @@
 'use strict';
 
-const express = require('express');
+const APIController = (function () {
+	const clientId = '';
+	const clientSecret = '';
 
-const app = express();
+	// private methods
+	const _getToken = async () => {
+		const result = await fetch('https://accounts.spotify.com/api/token', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded',
+				Authorization: 'Basic' + btoa(clientId + ':' + clientSecret),
+			},
+			body: 'grant_type=client_credentials',
+		});
 
-app.listen(8888, () =>
-  console.log(
-    'HTTP Server up. Now go to http://localhost:8888/login in your browser.'
-  )
-);
+		const data = await result.json();
+		return data.access_token;
+	};
+
+	const _getGenres = async (token) => {
+		const result = await fetch(
+			'https://api.spotify.com/v1/browse/categories?locale=sv_US',
+			{
+				method: 'GET',
+				headers: {
+					Authorization: 'Bearer ' + token,
+				},
+			}
+		);
+
+		const data = await result.json();
+		return data.categories.items;
+	};
+
+	const _getPlaylistByGenre = async (token, genreID) => {
+		const result = await fetch(
+			`https://api.spotify.com/v1/browse/categories/${genreID}/playlists?limit=${limit}`,
+			{
+				method: 'GET',
+				headers: {
+					Authorization: 'Bearer ' + token,
+				},
+			}
+		);
+
+		const data = await result.json();
+		return data.playlists.items;
+	};
+
+	const _getTracks = async (token, tracksEndPoint) => {
+		const list = 10;
+
+		const result = await fetch(`${tracksEndPoint}?limit=${limit}`, {
+			method: 'GET',
+			headers: {
+				Authorization: 'Bearer ' + token,
+			},
+		});
+
+		const data = await result.json();
+		return data.items;
+	};
+
+	const _getTrack = async (token, trackEndPoint) => {
+		const result = await fetch(`${trackEndPoint}`, {
+			method: 'GET',
+			headers: {
+				Authorization: 'Bearer ' + token,
+			},
+		});
+
+		const data = await result.json();
+		return data;
+	};
+
+	return {
+		_getToken() {
+			return _getToken();
+		},
+
+		_getGenres(token) {
+			return _getGenres(token);
+		},
+
+		_getPlaylistByGenre(token, genreID) {
+			return _getPlaylistByGenre(token, genreID);
+		},
+
+		_getTracks(token, tracksEndPoint) {
+			return _getTracks(token, tracksEndPoint);
+		},
+
+		_getTrack(token, trackEndPoint) {
+			return _getToken(token, trackEndPoint);
+		},
+	};
+})();
+
+// UI Module
+const UIController = function () {
+	// HTML selectors
+	const DOMElements = {};
+};
