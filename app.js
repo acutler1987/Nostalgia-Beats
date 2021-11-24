8656e134fcbc12bfae2e91d421a9796c570fde48
Alice Cutler <acutler0451@gmail.com>
2021-11-24 18:31:14 -0500

    11/24 6:31 commit

--- app.js
+++ app.js
@@ -99,7 +99,7 @@ const APIController = (function () {
 })();
 
 // UI Module
-const UIController = function () {
+const UIController = (function () {
 	// HTML selectors
 	const DOMElements = {
 		selectGenre: '',
@@ -122,5 +122,98 @@ const UIController = function () {
 				songDetail: document.querySelector(DOMElements.divSongDetail),
 			};
 		},
+
+		// method to create select list option
+		createGenre(text, value) {
+			const html = `html markup will go here, inserting ${text} and ${value}`;
+			document
+				.querySelector(DOMElements.selectGenre)
+				.insertAdjacentHTML('beforeend', html);
+		},
+
+		createPlaylist(text, value) {
+			const html = `html markup will go here, inserting ${text} and ${value}`;
+			document
+				.querySelector(DOMElements.selectPlaylist)
+				.insertAdjacentHTML('beforeend', html);
+		},
+
+		// method to create the track list
+		createTrack(id, name) {
+			const html = `html markup will go here, inserting ${id} and ${name}`;
+			document
+				.querySelector(DOMElements.divSongList)
+				.insertAdjacentHTML('beforeend', html);
+		},
+
+		// method to create song detail
+		createSongDetail(img, song, artist) {
+			const detailDiv = document.querySelector(DOMElements.divsongDetail);
+			// when user clicks on a song, clear out old details div
+			detailDiv.innerHTML = '';
+
+			const html = `markup goes here- ${img}, ${song} and ${artist} should each have their own divs`;
+
+			detailDiv.insertAdjacentHTML('beforeend', html);
+		},
+
+		resetTrackDetail() {
+			this.inputField().songDetail.innerHTML = '';
+		},
+
+		resetTracks() {
+			this.inputField().songs.innerHTML = '';
+			this.resetTrackDetail();
+		},
+
+		resetPlaylist() {
+			this.inputField().playlist.innerHTML = '';
+			this.resetTracks();
+		},
 	};
-};
+})();
+
+// the app proper
+const APPController = (function (UICtrl, APICtrl) {
+	// get input field object ref
+	const DOMInputs = UICtrl.inputField();
+
+	// get genres on page load
+	const loadGenres = async () => {
+		// get the token
+		const token = await APICtrl.getToken();
+		// get the genres
+		const genres = await APICtrl.getGenres(token);
+		// populate genre select element
+		genres.forEach((element) =>
+			UICtrl.createGenre(element.name, element.id)
+		);
+	};
+
+	// genre change event listener
+	DOMInputs.genre.addEventListener('change', async () => {
+		// when user changes genres, reset subsequent fields
+		UICtrl.resetPlaylist();
+
+		// get the token, add method to store the token on the page
+		const token = UICtrl.getStoredToken().token;
+
+		// get the genre select field
+		const genreSelect = UICtrl.inputField();
+
+		// get the selected genreID
+		const genreID = genreSelect.options[genreselect.selectedIndex].value;
+
+		// get playlist data from Spotify based on genre
+		const playlist = await APICtrl.getPlaylistByGenre(token, genreID);
+
+		// load the playlist select field
+		console.log(playlist);
+	});
+
+	// submit button click event listener
+	DOMInputs.submit.addEventListener('click', async (e) => {
+		// prevent page reset
+		e.preventDefault();
+	});
+})();
